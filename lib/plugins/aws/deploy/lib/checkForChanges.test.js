jacoza221/serverless

commit 05499e6083d4b36ba9b80b271b2becf4249dbbc6

fix: Fix changes detection when user package artifact is involved
(PR #7742)

lib/plugins/aws/deploy/lib/checkForChanges.js
@@ -133,6 +133,10 @@ module.exports = {
 
       // create hashes for all the zip files
       const zipFiles = globby.sync(['**.zip'], { cwd: serverlessDirPath, dot: true, silent: true });
+      if (this.serverless.service.package.artifact) {
+        const arficatPath = path.join(serverlessDirPath, this.serverless.service.package.artifact);
+        zipFiles.push(arficatPath);
+      }
       const zipFilePaths = zipFiles.map(zipFile => path.join(serverlessDirPath, zipFile));
 
       const readFile = BbPromise.promisify(fs.readFile);

lib/plugins/aws/deploy/lib/checkForChanges.test.js
@@ -604,6 +604,48 @@ describe('checkForChanges', () => {
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(true);
       });
     });
+
+    it('should not set a flag if the remote and local hashes are different for package.artifact', () => {
+      awsDeploy.serverless.service.package = {
+        artifact: 'foo/bar/my-own.zip',
+      };
+
+      globbySyncStub.returns([]);
+      cryptoStub
+        .createHash()
+        .update()
+        .digest.onCall(0)
+        .returns('hash-cf-template');
+      cryptoStub
+        .createHash()
+        .update()
+        .digest.onCall(1)
+        .returns('local-my-own-hash');
+
+      const input = [
+        { Metadata: { filesha256: 'hash-cf-template' } },
+        { Metadata: { filesha256: 'remote-my-own-hash' } },
+      ];
+
+      return expect(awsDeploy.checkIfDeploymentIsNecessary(input)).to.be.fulfilled.then(() => {
+        expect(normalizeCloudFormationTemplateStub).to.have.been.calledOnce;
+        expect(globbySyncStub).to.have.been.calledOnce;
+        expect(readFileStub).to.have.been.calledOnce;
+        expect(awsDeploy.serverless.cli.log).not.to.be.called;
+        expect(normalizeCloudFormationTemplateStub).to.have.been.calledWithExactly(
+          awsDeploy.serverless.service.provider.compiledCloudFormationTemplate
+        );
+        expect(globbySyncStub).to.have.been.calledWithExactly(['**.zip'], {
+          cwd: path.join(awsDeploy.serverless.config.servicePath, '.serverless'),
+          dot: true,
+          silent: true,
+        });
+        expect(readFileStub).to.have.been.calledWith(
+          path.join('my-service/.serverless/my-service/.serverless/foo/bar/my-own.zip')
+        );
+        expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
+      });
+    });
   });
 
   describe('#checkLogGroupSubscriptionFilterResourceLimitExceeded', () => {

commit 1ac67f15b414ac294045682f123b203cdfc23ab2

fix: Fix packaging logic after regression introduced with #7742 (#7743)

lib/plugins/aws/deploy/lib/checkForChanges.js
@@ -134,10 +134,9 @@ module.exports = {
       // create hashes for all the zip files
       const zipFiles = globby.sync(['**.zip'], { cwd: serverlessDirPath, dot: true, silent: true });
       if (this.serverless.service.package.artifact) {
-        const arficatPath = path.join(serverlessDirPath, this.serverless.service.package.artifact);
-        zipFiles.push(arficatPath);
+        zipFiles.push(this.serverless.service.package.artifact);
       }
-      const zipFilePaths = zipFiles.map(zipFile => path.join(serverlessDirPath, zipFile));
+      const zipFilePaths = zipFiles.map(zipFile => path.resolve(serverlessDirPath, zipFile));
 
       const readFile = BbPromise.promisify(fs.readFile);
       const zipFileHashesPromises = zipFilePaths.map(zipFilePath =>

lib/plugins/aws/deploy/lib/checkForChanges.test.js
@@ -308,7 +308,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });
@@ -351,7 +351,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });
@@ -389,7 +389,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });
@@ -432,8 +432,12 @@ describe('checkForChanges', () => {
           dot: true,
           silent: true,
         });
-        expect(readFileStub).to.have.been.calledWith(path.join('my-service/.serverless/func1.zip'));
-        expect(readFileStub).to.have.been.calledWith(path.join('my-service/.serverless/func2.zip'));
+        expect(readFileStub).to.have.been.calledWith(
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/func1.zip')
+        );
+        expect(readFileStub).to.have.been.calledWith(
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/func2.zip')
+        );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });
     });
@@ -474,7 +478,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });
@@ -512,7 +516,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(true);
       });
@@ -554,7 +558,7 @@ describe('checkForChanges', () => {
             silent: true,
           });
           expect(readFileStub).to.have.been.calledWith(
-            path.join('my-service/.serverless/my-service.zip')
+            path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/my-service.zip')
           );
           expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(true);
         }
@@ -599,8 +603,12 @@ describe('checkForChanges', () => {
           dot: true,
           silent: true,
         });
-        expect(readFileStub).to.have.been.calledWith(path.join('my-service/.serverless/func1.zip'));
-        expect(readFileStub).to.have.been.calledWith(path.join('my-service/.serverless/func2.zip'));
+        expect(readFileStub).to.have.been.calledWith(
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/func1.zip')
+        );
+        expect(readFileStub).to.have.been.calledWith(
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/func2.zip')
+        );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(true);
       });
     });
@@ -641,7 +649,7 @@ describe('checkForChanges', () => {
           silent: true,
         });
         expect(readFileStub).to.have.been.calledWith(
-          path.join('my-service/.serverless/my-service/.serverless/foo/bar/my-own.zip')
+          path.resolve(awsDeploy.serverless.config.servicePath, '.serverless/foo/bar/my-own.zip')
         );
         expect(awsDeploy.serverless.service.provider.shouldNotDeploy).to.equal(undefined);
       });